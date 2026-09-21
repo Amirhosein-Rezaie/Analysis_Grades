c4072de2cb1ba74383ca3bdadecaa9e928d29636
Amirhosein <ah.rezaie07@gmail.com>
2026-09-21 20:00:08 +0330

    make the class to work on the database.

---
+++ database/Database.py
@@ -0,0 +1,24 @@
+import sqlite3
+
+class Database:
+
+    # # properties
+    Connection = object()
+    Cursor = object()
+
+    # # funcs
+    def __init__(self, database_name:str):
+        self.Connection = sqlite3.connect(database_name)
+        self.Cursor = self.Connection.cursor()
+
+    # send a query to the database
+    def Get_Query(self, query:str):
+        self.Cursor.execute(query)
+
+    # run the queries that sent before
+    def Perform_Queries(self):
+        self.Connection.commit()
+
+    # close the connection of db
+    def Close_database(self):
+        self.Connection.close()
