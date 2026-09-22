79f838f4d2b925aea4ed27231e9e21bbb454fca2
Amirhosein <ah.rezaie07@gmail.com>
2026-09-22 22:09:58 +0330

    make the properties of Database class to private.

--- database/Database.py
+++ database/Database.py
@@ -3,24 +3,24 @@ import sqlite3
 class Database:
 
     # # properties
-    Connection = object()
-    Cursor = object()
+    __Connection = object()
+    __Cursor = object()
 
     # # funcs
     def __init__(self, database_name:str):
-        self.Connection = sqlite3.connect(database_name)
-        self.Cursor = self.Connection.cursor()
+        self.__Connection = sqlite3.connect(database_name)
+        self.__Cursor = self.__Connection.cursor()
 
     # send a query to the database
     def Get_Query(self, query:str, auto_commit:bool = True):
-        self.Cursor.execute(query)
+        self.__Cursor.execute(query)
 
         if auto_commit : self.Perform_Queries()
 
     # run the queries that sent before
     def Perform_Queries(self):
-        self.Connection.commit()
+        self.__Connection.commit()
 
-    # close the connection of db
+    # close the __Connection of db
     def Close_database(self):
-        self.Connection.close()
+        self.__Connection.close()
